ea62d0435b01fab0fc0e166a8998efd1d0f0caa7
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-10-28 02:31:57 +0000

    Bot logic and Views

--- bot.py
+++ bot.py
@@ -1,9 +1,57 @@
-import argparse
+# Importing the required packages
+from flask import Flask, request, render_template
+import telegram
+import os
+from nltk.chat.eliza import eliza_chatbot
 
-ap = argparse.ArgumentParser()
+# Bot credentials
+from botcontroller.credentials import BOT_TOKEN, BOT_USERNAME, URL
 
-ap.add_argument("-i", "--name", required = True, help = "Please enter name")
+# Initialize flask app
+app = Flask(__name__)
 
-args = vars(ap.parse_args())
+# Initialize telegram bot
+bot = telegram.Bot(token = BOT_TOKEN)
 
-print(args["name"])
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route(f"/{BOT_TOKEN}", methods = ["POST"])
+def respond():
+    """
+        Desc : This function defines the logic that controls how the telegram bot responds when a message is sent
+    """
+
+    # When a user sends a message in Telegram, we can receive the message as a JSON object and convert it to a Telegram object using the telegram module
+    new_message = telegram.Update.de_json(request.get_json(force = True), bot)
+
+    chat_id = new_message.message.chat_id
+    message_id = new_message.message.message_id
+
+    # Encoding text for unicode compatibility
+    text = new_message.message.text.encode("utf-8").decode()
+    print(f"[RECEIVED TEXT] : {text}")
+
+    # For a welcome message
+    if text == "/start":
+        welcome_msg = f"Hi {new_message.message.from_user.first_name}, I'm Toyosi - The favourite mental health bot. Let's talk about your issues, Be free with me"
+        bot.sendMessage(chat_id = chat_id, reply_to_message_id = message_id, text = welcome_msg)
+
+    else:
+        bot.sendMessage(chat_id = chat_id, reply_to_message_id = message_id, text = eliza_chatbot.respond(text))
+
+    return ""
+
+@app.route("/set_webhook")
+def setWebhook():
+    """
+        Desc : This webhook enables the bot to run once the server is invoked
+    """
+
+    hook = bot.setWebhook(os.path.join(URL, BOT_TOKEN))
+    if hook:
+        return "Webhook successfully set."
+    return "Webhook configuration failed."
+
+if __name__ == "__main__":
+    app.run(debug = True, threaded = True)
---
+++ botcontroller/credentials.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "Enter your Generated bot http token here"
+
+BOT_USERNAME = "Enter given Bot name"
+
+URL = "Public URL - Ensure it is https to ensure compatibility with the WebHook"
